0341169cc154dd1fa1155a3d22641a1b0107c74d
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2022-11-23 11:51:55 +0000

    Moved javascript out of html and into its own file

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+$(".banner h1").textillate({
+    in: {
+      effect: "tada",
+    }
+  });
+  $(".btn-footer").on("click", function() {
+    $(".banner h1").textillate("in");
+  });
+  $(".sidebar h1").on("click", function() {
+    $(".banner h1").textillate("in");
+  });
