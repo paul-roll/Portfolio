b9e7d85df5eb21a2db6e0f37bbed55747748b56c
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2022-11-23 12:01:44 +0000

    indent fix

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,13 @@
 $(".banner h1").textillate({
-    in: {
-      effect: "tada",
-    }
-  });
-  $(".btn-footer").on("click", function() {
-    $(".banner h1").textillate("in");
-  });
-  $(".sidebar h1").on("click", function() {
-    $(".banner h1").textillate("in");
-  });
+  in: {
+    effect: "tada",
+  }
+});
+$(".btn-footer").on("click", function() {
+  $(".banner h1").textillate("in");
+});
+$(".sidebar h1").on("click", function() {
+  $(".banner h1").textillate("in");
+});
 
-  $(".btn-burger").removeClass("hidden");
+$(".btn-burger").removeClass("hidden");
