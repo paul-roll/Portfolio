934948c4be8386dc018b6e1aa66910bc1972b041
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2022-11-23 14:15:22 +0000

    Added working toggle to burger button Cleaned up some inconsistencies between HTML pages

--- js/javascript.js
+++ js/javascript.js
@@ -15,3 +15,15 @@ $(".btn-burger").removeClass("hidden");
 $(".btn-burger").on("click", function() {
   $("nav").toggleClass("hidden");
 });
+
+$(".sidebar").on("click", function() {
+  if (!$(".sidebar").hasClass("hidden")) {
+    $(".sidebar").addClass("hidden")
+  }
+});
+
+$("body").on("click", function(event) {
+  if ((!$(event.target).parent().hasClass("btn-burger")) && (!$(".sidebar").hasClass("hidden"))) {
+    $(".sidebar").addClass("hidden");
+  }
+})
