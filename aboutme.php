0e3c15fc2e0f329c740e50e7df4b7b65d260f492
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2023-01-18 11:40:05 +0000

    updated text on about me page

--- aboutme.php
+++ aboutme.php
@@ -8,12 +8,14 @@
                 <div class="wrapper">
                     <a id="aboutme"></a>
                     <h2>About Me</h2>
-                    <p>I'm Paul, a trainee web developer.</p>
-                    <p>Currently studying on the Netmatters Scion Coalition Scheme, working towards a career in software development.</p>
-                    <p><span class="bold">Skills:&nbsp;</span> HTML, CSS, SASS, JavaScript, SQL</p>
+                    <p>I'm Paul, a dedicated and friendly developer with a lifelong interest in IT.</p>
+                    <p>For years I've had an amateur interest in coding often pulling video-games apart and tweaking scripts in languages like Visual Basic, Lua and C# ...</p>
+                    <p>Recently, I enjoyed an opportunity training in the Netmatters Scion Coalition Scheme where I've gained a lot more proficiency and confidence with not just programming, but development as a whole.</p>
+                    <p>I am very much looking forward to securing a junior role in a software development career, where I can grow as a developer and contribute towards some awesome projects!</p>
+                    <p><span class="bold">Skills:&nbsp;</span> HTML, CSS, SASS, JavaScript, PHP, SQL</p>
                     <img src="img/profile.jpg" alt="Profile Picture">
                     <h2>My Interests</h2>
-                    <p>I love video games, and I've always wanted to work in software development because of my passion for tweaking and editing them.</p>
+                    <p>I've always loved most kinds of gaming, especially shared experiences and working together to tackle challenges or solve problems, it's great using them as a platform to socialize with friends. I enjoy getting to understand how their mechanics work and whenever possible end up tweaking and modding them to create a better experience.</p>
                 </div>
             </section>
 <?php
