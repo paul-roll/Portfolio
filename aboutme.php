0e988409d335a7c3a5df4da36e3338e51ee98929
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2023-01-27 10:16:07 +0000

    Updated aboutme with content from CV - added extra margin above h2 on aboutme page

--- aboutme.php
+++ aboutme.php
@@ -1,25 +1,26 @@
 <?php
-	include("inc/header.php");
+include("inc/header.php");
 ?>
-        
-        <main>
 
-            <section class="aboutme">
-                <div class="wrapper">
-                    <a id="aboutme"></a>
-                    <h2>About Me</h2>
-                    <p>I'm Paul, a dedicated and friendly developer with a lifelong interest in IT.</p>
-                    <p>For years I've had an amateur interest in coding often pulling video-games apart and tweaking scripts in languages like Visual Basic, Lua and C# ...</p>
-                    <p>Recently, I enjoyed an opportunity training in the Netmatters Scion Coalition Scheme where I've gained a lot more proficiency and confidence with not just programming, but development as a whole.</p>
-                    <p>I am very much looking forward to securing a junior role in a software development career, where I can grow as a developer and contribute towards some awesome projects!</p>
-                    <p><span class="bold">Skills:&nbsp;</span> HTML, CSS, SASS, JavaScript, PHP, SQL</p>
-                    <img src="img/profile.jpg" alt="Profile Picture">
-                    <h2>My Interests</h2>
-                    <p>I've always loved most kinds of gaming, especially shared experiences and working together to tackle challenges or solve problems, it's great using them as a platform to socialize with friends. I enjoy getting to understand how their mechanics work and whenever possible end up tweaking and modding them to create a better experience.</p>
-                </div>
-            </section>
-<?php
+<main>
+
+    <section class="aboutme">
+        <div class="wrapper">
+            <a id="aboutme"></a>
+            <h2>About Me</h2>
+            <p>I am a hard-working and responsible developer with a lifelong passion for IT, working well in both a team environment and on my own initiative. For years I've had an amateur interest in coding often pulling video-games apart and tweaking scripts in languages like Visual Basic, Lua and C# ...</p>
+            <p>Recently, I enjoyed an opportunity training in the Netmatters Scion Coalition Scheme where I've gained a lot more proficiency and confidence with not just programming, but development as a whole while reinforcing my adaptability to challenges and reliable time keeping skills.</p>
+            <p>I am enthusiastic, self-motivated and very much looking forward to securing a junior role in a software development career, where I can grow as a developer and contribute towards some awesome projects!</p>
+            <p><span class="bold">Skills:&nbsp;</span> HTML5, CSS3, JavaScript, PHP, SQL, Git, Laravel</p>
+            <img src="img/profile.jpg" alt="Profile Picture">
+            <h2>Hobbies</h2>
+            <h3>Programming</h3>
+            <p>Fascinated with the development of interactive and creative software, often tweaking scripts in languages like Visual Basic and Lua to enhance one game or another. Recently, experimenting in Unity and taking advantage of its C# integration with Virtual Reality devices.</p>
+            <h3>Game Servers</h3>
+            <p>Have been responsible for the setup and running of numerous dedicated private servers for games such as Minecraft and Ark. Using remote-console management, crash-logs to diagnose and fix world corruption, and being trusted as a responsible admin.</p>        </div>
+    </section>
+    <?php
     include("inc/contact.php");
-	include("inc/footer.php");
-	include("inc/footer2.php");
-?>
+    include("inc/footer.php");
+    include("inc/footer2.php");
+    ?>
