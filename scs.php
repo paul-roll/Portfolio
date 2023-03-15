0e7f56ce9a12dcdc115a9fdc7b2b9d134f9030da
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2023-03-15 13:02:40 +0000

    Fixed SCS description to reflect SCSB differences.

--- scs.php
+++ scs.php
@@ -9,7 +9,7 @@
                     <a id="scs"></a>
 
                     <h2>Introduction to Scion Coalition Scheme</h2>
-                    <p>The Scion Coalition Scheme is an intensive, specially tailored training program run by Netmatters in order to give willing candidates the opportunity to enter the industry as web developers. Under the supervision of senior web developers, scions generally aim to complete training within six to nine months. The course is intensive and therefore the level of learning achieved is extensive in a short space of time.</p>
+                    <p>The Scion Coalition Scheme Boot Camp is an intensive, specially tailored training program being run by Netmatters in order to give willing candidates the opportunity to enter the industry as web developers. Under the supervision of senior web developers, scions generally aim to complete training within sixteen weeks. The course is intensive and therefore the level of learning achieved is extensive in a short space of time.</p>
 
                     <h2>Treehouse<img src="img/treehouse.jpg" alt=""></h2>
                     <p>Treehouse is an online learning community, featuring videos covering a number of topics from basic HTML to C# programming, iOS development, data analysis, and more. By completing courses users can earn points, allowing them to track their progress and see how much they've covered in certain areas.</p>
